1d651dde8ec536ee8aa902223ff4ff5b934e0bfb
Pratham Kwatra <pratham.kwatra@drexel.edu>
2023-06-29 00:08:11 -0400

    week 1 start

---
+++ gotutorial/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	RunBasicIODemo()
+	RunConditionalDemo()
+	RunTypesAndInterfacesDemo()
+	RunConstantsDemo()
+	RunTypeAliasDemo()
+	RunArraySliceMapDemo()
+	RunLoopDemo()
+	RunFunctionsDemo()
+	RunBasicPointersDemo()
+	RunStructsDemo()
+	RunObjectReceiverDemo()
+	RunErrorsDemo()
+	RunOopDemo()
+	RunInterfaceDemo()
+	RunFileIODemo()
+	RunJsonDemo()
+}
